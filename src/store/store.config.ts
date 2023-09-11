734cc475707a5d8303656687fb0209b33777ee9d
alexiasouvane <alexia.souvane@gmail.com>
2023-09-11 07:54:41 +0200

    add ReduxStore

--- src/store/store.config.ts
+++ src/store/store.config.ts
@@ -1,4 +1,4 @@
-import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit";
+import { Action, Store, ThunkAction, ThunkDispatch, configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import { Todo, TodolistSlice } from "./todolist.reducer";
 import { TodolistWebserviceInterface } from "../infra/todolist.webservice.interface";
@@ -28,3 +28,6 @@ export interface Dependencies {
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export type ThunkResult<R> = ThunkAction<R, RootState, Dependencies, Action>;
+export type ReduxStore = Store<RootState> & {
+  dispatch: ThunkDispatch<RootState, Dependencies, Action>;
+};
