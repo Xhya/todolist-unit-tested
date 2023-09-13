4c5ac453145fc0da885394165fad43d15b64c7e5
alexiasouvane <alexia.souvane@gmail.com>
2023-09-13 12:49:55 +0200

    --amend

--- src/infra/todolist.webservice.mock.ts
+++ src/infra/todolist.webservice.mock.ts
@@ -2,13 +2,13 @@ import { Todo } from "../store/todolist.reducer";
 import { TodolistWebserviceInterface } from "./todolist.webservice.interface";
 
 export class TodolistWebserviceMock implements TodolistWebserviceInterface {
-  private _list = {
-    0: {
+  private _list = [
+    {
       id: 0,
       text: "Salade",
       completed: false,
     },
-  };
+  ];
 
   getList(): Promise<any> {
     return new Promise((resolve) => {
